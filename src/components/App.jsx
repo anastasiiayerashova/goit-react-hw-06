fb9a829855b82e188fd606b271ad17c44bb6e51d
Anastasiia Yerashova <yerashova.a@gmail.com>
2024-11-27 14:58:57 +0100

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ContactForm from './ContactForm/ContactForm'
+
+export default function App() {
+  return (
+    <div className='mainWrapper'> 
+      <ContactForm />
+    </div>
+ )
+}
+
+
